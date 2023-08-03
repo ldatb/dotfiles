25d6e03c7ff1e27fcf20b6f32fbf71e9a280b7d9
Lucas de Ataides <lucasatab@gmail.com>
2023-08-03 10:24:27 -0300

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -221,6 +221,61 @@ return require('packer').startup(function(use)
     -- multiple cursors
     use 'mg979/vim-visual-multi'
 
+    -- barbar
+    use 'nvim-tree/nvim-web-devicons'
+    use {
+      'romgrk/barbar.nvim',
+      config = function()
+        require("barbar").setup {
+          animation = true,
+          auto_hide = false,
+          tabpages = false,
+          clickable = true,
+          insert_at_end = true,
+          icons = {
+            buffer_index = true,
+            buffer_number = false,
+            button = '',
+
+            gitsigns = {
+              added = {enabled = true, icon = '+'},
+              changed = {enabled = true, icon = '~'},
+              deleted = {enabled = true, icon = '-'},
+            },
+
+            filetype = {
+              -- Sets the icon's highlight group.
+              -- If false, will use nvim-web-devicons colors
+              custom_colors = false,
+
+              -- Requires `nvim-web-devicons` if `true`
+              enabled = true,
+            },
+
+            separator = {left = '▎', right = ''},
+
+            -- If true, add an additional separator at the end of the buffer list
+            separator_at_end = true,
+
+            -- Configure the icons on the bufferline when modified or pinned.
+            -- Supports all the base icon options.
+            modified = {button = '●'},
+            pinned = {button = '', filename = true},
+
+            -- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+            preset = 'default',
+
+            -- Configure the icons on the bufferline based on the visibility of a buffer.
+            -- Supports all the base icon options, plus `modified` and `pinned`.
+            alternate = {filetype = {enabled = false}},
+            current = {buffer_index = true},
+            inactive = {button = '×'},
+            visible = {modified = {buffer_number = false}},
+          },
+        }
+      end
+    }
+
     -- tokyo night theme
     use 'folke/tokyonight.nvim'
 end)
